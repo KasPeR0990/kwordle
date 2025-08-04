eeca6e884706359da916ab9899620596774c428a
Kasper Hallenius <kasperhallenius@icloud.com>
2025-08-04 13:58:34 +0200

    typing in tiles

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,34 @@ export default function App() {
   const [guesses, setGuesses] = useState(Array(6).fill(null));
 
   useEffect(() => {
-    const handleType = (event: KeyboardEvent) => {};
+    const handleType = (event: KeyboardEvent) => {
+      if (event.key === "Enter" && currentGuess.length === WORD_LENGTH) {
+        event.preventDefault();
+        const index = guesses.findIndex((g) => g === null);
+
+        if (index !== -1) {
+          const nextGuesses = [...guesses];
+          nextGuesses[index] = currentGuess;
+          setGuesses(nextGuesses);
+          setCurrentGuess("");
+        }
+        return;
+      }
+
+      if (event.key === "Backspace" && currentGuess.length > 0) {
+        setCurrentGuess((prev) => prev.slice(0, -1));
+      }
+
+      if (!/^[a-zA-Z]$/.test(event.key)) return;
+
+      if (currentGuess.length !== WORD_LENGTH) {
+        setCurrentGuess((prev) => prev + event.key.toLowerCase());
+      }
+    };
 
     window.addEventListener("keydown", handleType);
-
     return () => window.removeEventListener("keydown", handleType);
-  }, []);
+  }, [currentGuess]);
 
   useEffect(() => {
     const fetchWords = async () => {
@@ -38,8 +60,13 @@ export default function App() {
       <div>{solution}</div>
       <div className="built-by">built by kasper</div>
       <div className="board">
-        {guesses.map((guess) => {
-          return <Line guess={guess} />;
+        {guesses.map((guess, i) => {
+          //
+          const isCurrentGuess = i === guesses.findIndex((val) => val === null);
+
+          return (
+            <Line guess={isCurrentGuess ? currentGuess : guess ?? ""} key={i} />
+          );
         })}
       </div>
     </div>
